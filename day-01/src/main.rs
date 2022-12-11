e2f5140d76323f5c91d95c8b028cf8e6062e6f32
Evan Dower <edower@indeed.com>
2022-12-11 08:23:43 -0800

    Day01.1: Solution that satisfies the example for Part 1
    Uses a BufReader to avoid reading the whole file into memory (in case of large files).

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
 use color_eyre::eyre::Context;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Lines},
+};
 
 /// Solver for Advent of Code Day 1 (2022, Rust)
 #[derive(Parser, Debug)]
@@ -15,13 +19,36 @@ fn main() -> color_eyre::Result<()> {
 
     let args = Args::parse();
 
-    let input = read_input(args.path)?;
-    println!("{input}");
+    let lines = read_lines(args.path)?;
+    let mut max: Option<u64> = None;
+    let mut sum: u64 = 0;
+
+    for line in lines {
+        let line = line.wrap_err("reading line")?;
+        match line.is_empty() {
+            false => {
+                // We have a line with a number on it, so add it to the elf's running total.
+                sum = sum
+                    + line
+                        .parse::<u64>()
+                        .wrap_err(format!("while parsing {line} as u64"))?
+            }
+            true => {
+                // We have a blank line, so update our max and reset sum for the next elf.
+                max = std::cmp::max(max, Some(sum));
+                sum = 0
+            }
+        }
+    }
+    max = std::cmp::max(max, Some(sum));
+
+    println!("{max:?}");
 
     Ok(())
 }
 
-fn read_input(path: std::path::PathBuf) -> color_eyre::Result<String> {
-    let input = std::fs::read_to_string(&path).wrap_err(format!("reading {}", path.display()))?;
-    Ok(input)
+fn read_lines(path: std::path::PathBuf) -> color_eyre::Result<Lines<BufReader<File>>> {
+    let file = std::fs::File::open(&path).wrap_err(format!("while opening {}", path.display()))?;
+    let lines = std::io::BufReader::new(file).lines();
+    Ok(lines)
 }
