930e708d1a82fbe07069904ddedb7046100f1d81
Evan Dower <edower@indeed.com>
2022-12-11 07:10:25 -0800

    Day01.1: Basic arg-parsing and file-handling

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use color_eyre::eyre::Context;
+
+/// Solver for Advent of Code Day 1 (2022, Rust)
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// path to a file containing calorie counts for each Elf's food, with one food per line and blank lines separating elves
+    #[arg(short, long)]
+    path: std::path::PathBuf,
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let args = Args::parse();
+
+    let input = read_input(args.path)?;
+    println!("{input}");
+
+    Ok(())
+}
+
+fn read_input(path: std::path::PathBuf) -> color_eyre::Result<String> {
+    let input = std::fs::read_to_string(&path).wrap_err(format!("reading {}", path.display()))?;
+    Ok(input)
 }
